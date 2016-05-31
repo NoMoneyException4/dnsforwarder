638c0e819279d7da7eb8b9e8f9d8b48b00e89147
Frank Yang <codebear4@gmail.com>
2016-05-31 11:30:30 +0800

    Add doc.go
    Signed-off-by: Frank Yang <codebear4@gmail.com>

---
+++ doc.go
@@ -0,0 +1,16 @@
+/*
+Dns Forwader, a dns server, cacher, and forwarder.
+
+
+Usage:
+	sudo dnsforwader [options]
+
+	Use sudo command to gain the root privileges for dns default port 53.
+
+Options:
+	-c Configuration file path (default: ./dnsforwarder.yml)
+	-h Server host (default: 127.0.0.1)
+	-p Server port (default: 53)
+	-d Enable debug mode (default:false)
+*/
+package main
